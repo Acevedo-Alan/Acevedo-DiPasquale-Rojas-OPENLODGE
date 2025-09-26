1d724b30bba47896d1985ece214d497e588856ca
Maxi <maximorojas002@gmail.com>
2025-09-26 13:04:00 -0300

    ready para el pull request

--- frontend/scripts/footer.js
+++
@@ -1,8 +0,0 @@
-const footer = document.querySelector(".footer");
-footer.innerHTML = `
-    <nav class="nav-footer">
-        <ul class="menu-footer">
-            <li>© OpenLodge 2025</li>
-        </ul>
-    </nav>
-`;
