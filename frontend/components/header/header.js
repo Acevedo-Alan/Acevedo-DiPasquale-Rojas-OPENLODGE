0c9054dc36cd7ba9c9c76002819a20e60bafd08b
MaxLRxd <maximorojas002@gmail.com>
2025-09-26 01:32:41 -0300

    forms de modificacion, reserva y creacion de alojamientos agregados

--- frontend/components/header/header.js
+++ frontend/components/header/header.js
@@ -1,6 +1,6 @@
 const header = document.querySelector(".header");
 
-const userRole = "ANFITRION";
+const userRole = "HUESPED";
 
 if (userRole === "ANFITRION") {
     header.innerHTML = `
@@ -12,8 +12,8 @@ if (userRole === "ANFITRION") {
             
         <ul class="nav-center">
             <li><a href="/pages/index/index.html">Inicio</a></li>
-            <li><a href="/pages/perfi/perfil.html">Perfil</a></li>
-            <li><a href="/pages/agregarPropiedad/formulario-registro-propiedad.html">Agregar propiedad</a></li>
+            <li><a href="/pages/perfil/perfil.html">Perfil</a></li>
+            <li><a href="/pages/agregarPropiedad/agregarPropiedad.html">Agregar propiedad</a></li>
         </ul>
 
         <Button class="mobile-menu-toggle" id="mobileToggle">
@@ -26,15 +26,15 @@ if (userRole === "ANFITRION") {
     `;
 } else if (userRole === "HUESPED") {
     header.innerHTML = `
-    <nav class="nav">
+    <nav class="nav" id="nav">
         <div class="logo-container">
             <div class="logo"></div>
             <p>OpenLodge</p>
         </div>
-        <ul class="menu">
-            <li class="item"><a href="#">inicio</a></li>
-            <li class="item"><a href="#">perfil</a></li>
-            <li class="item"><a href="#">sobre nosotros</a></li>
+        <ul class="nav-center">
+            <li><a href="/pages/index/index.html">inicio</a></li>
+            <li><a href="/pages/perfil/perfil.html">perfil</a></li>
+            <li><a href="#">sobre nosotros</a></li>
         </ul>
         <Button class="mobile-menu-toggle" id="mobileToggle">
             <span></span>
@@ -52,16 +52,14 @@ if (userRole === "ANFITRION") {
             </div>
             
             <ul class="nav-center">
-                <li><a href="#inicio">Inicio</a></li>
-                <li><a href="#destinos">Destinos</a></li>
-                <li><a href="#experiencias">Experiencias</a></li>
-                <li><a href="#ofertas">Ofertas</a></li>
-            </ul>
-            
-            <div class="nav-actions">
+                <li><a href="/pages/index/index.html">Inicio</a></li>
+                <div class="nav-actions">
                 <a href="/pages/autenticacion/login/login.html">Iniciar sesión</a>
                 <button class="btn-cta">Reservar ahora</button>
             </div>
+            </ul>
+            
+           
             
             <Button class="mobile-menu-toggle" id="mobileToggle">
                 <span></span>
