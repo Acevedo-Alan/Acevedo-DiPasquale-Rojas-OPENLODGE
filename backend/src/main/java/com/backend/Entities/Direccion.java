ce6d743da8c34a22e858aecfdf1a40f2a2fc1c92
MaxLRxd <maximorojas002@gmail.com>
2025-10-28 20:24:36 -0300

    correccion de cositas

--- backend/src/main/java/com/backend/Entities/Direccion.java
+++ backend/src/main/java/com/backend/Entities/Direccion.java
@@ -1,10 +1,24 @@
 package com.backend.Entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "direccion")
 public class Direccion {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "calle")
+    private String calle;
 }
--- backend/src/main/java/com/backend/Security/JwtUtil.java
+++ backend/src/main/java/com/backend/Security/JwtUtil.java
@@ -2,16 +2,21 @@ package com.backend.Security;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.function.Function;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtUtil {
+    private static final String SECRET_KEY ="Y2xhdmUtc2VjcmV0YS1tdXktbGFyZ2EtcGFyYS1hcGxpY2FjaW9uLXNwcmluZy1ib290";
     private final Key clave = Keys.secretKeyFor(SignatureAlgorithm.HS256);
     private final long expiration = 360000;
 
@@ -33,12 +38,35 @@ public class JwtUtil {
         }
     }
 
-    public String getUsernameFromToken(String token){
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    public String getUsernameFromToken(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractClaim(token, Claims::getExpiration).before(new Date());
+    }
+    
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
         return Jwts.parserBuilder()
-                .setSigningKey(clave)
+                .setSigningKey(getSigningKey())
                 .build()
                 .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
+                .getBody();
     }
+    
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
 }
